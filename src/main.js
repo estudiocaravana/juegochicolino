60ac450143de75cac180566a9a726e76c824a4c3
edupoch <edupoch@gmail.com>
2026-05-04 20:36:11 +0200

    Los elementos crecen y se giran un poco

--- src/main.js
+++ src/main.js
@@ -5,12 +5,23 @@ import { animate, svg, stagger } from "animejs";
 document.querySelectorAll("#Playa > g, #Juegos > g").forEach((group) => {
   group.addEventListener("mouseover", () => {
     animate(group, {
-      y: [{ to: "-1rem", ease: "outElastic(1, 0.3)", duration: 1000 }],
+      // y: [{ to: "-1rem", ease: "outElastic(1, 0.3)", duration: 1000 }],
+      scale: [{ to: "1.1", ease: "outElastic(1, 0.3)", duration: 1000 }],
+      zIndex: [{ to: "100", duration: 1 }],
+    });
+    animate(group, {
+      rotate: "5deg",
+      ease: "inOutSine",
+      alternate: true,
+      loop: true,
+      duration: 2000,
     });
   });
   group.addEventListener("mouseout", () => {
     animate(group, {
-      y: [{ to: "0", ease: "outElastic(1, 0.3)", duration: 1000 }],
+      // y: [{ to: "0", ease: "outElastic(1, 0.3)", duration: 1000 }],
+      scale: [{ to: "1", ease: "outElastic(1, 0.3)", duration: 1000 }],
+      rotate: [{ to: "0", ease: "inOutSine", duration: 100 }],
     });
   });
 });
