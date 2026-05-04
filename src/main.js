e07b2239fdde935ced1f3da260f9846bb0f31e98
edupoch <edupoch@gmail.com>
2026-05-04 19:16:00 +0200

    Movimiento de spring en hover

--- src/main.js
+++ src/main.js
@@ -5,14 +5,12 @@ import { animate, svg, stagger } from "animejs";
 document.querySelectorAll("#Playa > g, #Juegos > g").forEach((group) => {
   group.addEventListener("mouseover", () => {
     animate(group, {
-      y: [{ to: "-1rem", ease: "outBounce", duration: 300 }],
+      y: [{ to: "-1rem", ease: "outElastic(1, 0.3)", duration: 1000 }],
     });
   });
   group.addEventListener("mouseout", () => {
     animate(group, {
-      y: 0,
-      duration: 500,
-      easing: "outBounce",
+      y: [{ to: "0", ease: "outElastic(1, 0.3)", duration: 1000 }],
     });
   });
 });
