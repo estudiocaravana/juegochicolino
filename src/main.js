8c054faa98fb632dbcfbf8394dcbcbe05392bad0
edupoch <edupoch@gmail.com>
2026-05-05 11:45:33 +0200

    Sincronizamos las animaciones de click

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-import { animate, svg, stagger } from "animejs";
+import { animate, createTimeline, svg, stagger } from "animejs";
 
 document.querySelectorAll("#Playa > g, #Juegos > g").forEach((group) => {
   let cuadradoDibujado = false;
@@ -40,6 +40,7 @@ document.querySelectorAll("#Playa > g, #Juegos > g").forEach((group) => {
   group.addEventListener("click", () => {
     group.classList.remove("seleccionado");
     if (!cuadradoDibujado) {
+      cuadradoDibujado = true;
       // Añade un cuadrado que rodea a cada grupo
       const bbox = group.getBBox();
       rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
@@ -62,55 +63,109 @@ document.querySelectorAll("#Playa > g, #Juegos > g").forEach((group) => {
 
       // Colocamos el grupo al final del padre para que quede por encima de los demás
       group.parentNode.appendChild(group);
+
+      // Colocamos el grupo en el centro de la pantalla
+      const groupRect = group.getBoundingClientRect();
+      const centerY = window.innerHeight / 2;
+      const centerX = window.innerWidth / 2;
+      const offsetY = centerY - (groupRect.top + groupRect.height / 2);
+      const offsetX = centerX - (groupRect.left + groupRect.width / 2);
+
+      const tl = createTimeline({ defaults: { duration: 750 } });
+
+      tl.label("start")
+        .add(
+          group,
+          {
+            y: [
+              {
+                to: offsetY,
+                ease: "inOutQuad",
+                duration: 500,
+              },
+            ],
+            x: [
+              {
+                to: offsetX,
+                ease: "inOutQuad",
+                duration: 500,
+              },
+            ],
+            scale: [{ to: "2", ease: "outElastic(1, 0.3)", duration: 1000 }],
+          },
+          "start",
+        )
+        .add(
+          drawable,
+          {
+            draw: ["0 0", "0 1"],
+            ease: "inOutQuad",
+            duration: 500,
+            delay: stagger(100),
+          },
+          200,
+        )
+        .add(
+          rect,
+          {
+            fill: "#ffffffff",
+            ease: "inOutQuad",
+            duration: 500,
+          },
+          400,
+        );
     } else {
-      // Devolvemos el grupo a su posición original
-      const referenceNode = group.parentNode.children[originalIndex] || null;
-      group.parentNode.insertBefore(group, referenceNode);
+      const tl = createTimeline({ defaults: { duration: 750 } });
+
+      tl.label("start")
+        .add(
+          drawable,
+          {
+            draw: ["0 1", "0 0"],
+            ease: "inOutQuad",
+            duration: 300,
+            // delay: stagger(100),
+          },
+          "start",
+        )
+        .add(
+          rect,
+          {
+            fill: "#ffffff00",
+            ease: "inOutQuad",
+            duration: 500,
+          },
+          200,
+        )
+        .add(
+          group,
+          {
+            y: [
+              {
+                to: 0,
+                ease: "inOutQuad",
+                duration: 500,
+              },
+            ],
+            x: [
+              {
+                to: 0,
+                ease: "inOutQuad",
+                duration: 500,
+              },
+            ],
+            scale: [{ to: "1", ease: "outElastic(1, 0.3)", duration: 1000 }],
+          },
+          300,
+        )
+        .call(() => {
+          // Devolvemos el grupo a su posición original
+          const referenceNode =
+            group.parentNode.children[originalIndex] || null;
+          group.parentNode.insertBefore(group, referenceNode);
+          rect.remove();
+          cuadradoDibujado = false;
+        }, 300);
     }
-
-    // Colocamos el grupo en el centro de la pantalla
-    const groupRect = group.getBoundingClientRect();
-    const centerY = window.innerHeight / 2;
-    const centerX = window.innerWidth / 2;
-    const offsetY = centerY - (groupRect.top + groupRect.height / 2);
-    const offsetX = centerX - (groupRect.left + groupRect.width / 2);
-
-    animate(group, {
-      y: [
-        {
-          to: cuadradoDibujado ? 0 : offsetY,
-          ease: "inOutQuad",
-          duration: 500,
-        },
-      ],
-      x: [
-        {
-          to: cuadradoDibujado ? 0 : offsetX,
-          ease: "inOutQuad",
-          duration: 500,
-        },
-      ],
-      scale: [{ to: "2", ease: "inOutQuad", duration: 500 }],
-      zIndex: [{ to: "1000", duration: 1 }],
-    });
-
-    animate(drawable, {
-      draw: cuadradoDibujado ? ["0 1", "0 0"] : ["0 0", "0 1"],
-      ease: "inOutQuad",
-      duration: 500,
-      delay: stagger(100),
-    });
-    animate(rect, {
-      fill: cuadradoDibujado ? "#ffffff00" : "#ffffffff",
-      ease: "inOutQuad",
-      duration: 500,
-    });
-    // Borramos el rectángulo después de la animación si se está ocultando
-    if (cuadradoDibujado) {
-      setTimeout(() => {
-        rect.remove();
-      }, 500);
-    }
-    cuadradoDibujado = !cuadradoDibujado;
   });
 });
