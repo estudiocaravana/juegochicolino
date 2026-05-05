94e239246b8c50ff446605bf05a9c74a7d9c50d5
edupoch <edupoch@gmail.com>
2026-05-05 12:37:35 +0200

    Animación de objeto incorrecto

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ document.querySelectorAll("#Playa > g, #Juegos > g").forEach((group) => {
   let svgPadre = group.closest("svg");
   let padre = group.parentNode;
 
-  let cuadradoDibujado = false;
+  let animandoObjeto = false;
   let originalIndex;
   let drawable;
   let rect;
 
   group.addEventListener("mouseover", () => {
-    if (!cuadradoDibujado) {
+    if (!animandoObjeto) {
       group.classList.add("seleccionado");
       animate(group, {
         // y: [{ to: "-1rem", ease: "outElastic(1, 0.3)", duration: 1000 }],
@@ -30,7 +30,7 @@ document.querySelectorAll("#Playa > g, #Juegos > g").forEach((group) => {
   });
 
   group.addEventListener("mouseout", () => {
-    if (!cuadradoDibujado) {
+    if (!animandoObjeto) {
       group.classList.remove("seleccionado");
       animate(group, {
         // y: [{ to: "0", ease: "outElastic(1, 0.3)", duration: 1000 }],
@@ -43,135 +43,172 @@ document.querySelectorAll("#Playa > g, #Juegos > g").forEach((group) => {
   // Al hacer clic, coloreamos el rectángulo de fondo
   group.addEventListener("click", () => {
     group.classList.remove("seleccionado");
-    if (!cuadradoDibujado) {
-      cuadradoDibujado = true;
-      // Añade un cuadrado que rodea a cada grupo
-      const bbox = group.getBBox();
-      rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-      rect.setAttribute("class", "rectangulo");
-      rect.setAttribute("x", bbox.x - 10);
-      rect.setAttribute("y", bbox.y - 10);
-      rect.setAttribute("width", bbox.width + 20);
-      rect.setAttribute("height", bbox.height + 20);
-      rect.setAttribute("fill", "#ffffff00"); // Transparente
-      rect.setAttribute("pointer-events", "all");
-      rect.setAttribute("stroke-width", 2);
-      rect.setAttribute("stroke", "#000000");
-      // Colocamos el rect al comienzo del grupo para que quede debajo de los elementos
-      group.insertBefore(rect, group.firstChild);
 
-      drawable = svg.createDrawable(rect);
+    if (group.classList.contains("incorrecto")) {
+      // OBJETO INCORRECTO
 
-      // Guardamos la posición de hijo original del grupo con respecto al padre
-      originalIndex = Array.from(group.parentNode.children).indexOf(group);
-
-      // Colocamos el grupo al final del padre para que quede por encima de los demás
-      group.parentNode.appendChild(group);
-
-      // Colocamos el grupo en el centro de la pantalla
-      const groupRect = group.getBoundingClientRect();
-      const centerY = window.clientHeight / 2;
-      const centerX = window.clientWidth / 2;
-      const offsetY = centerY - (groupRect.top + groupRect.height / 2);
-      const offsetX = centerX - (groupRect.left + groupRect.width / 2);
-
+      // Primero añadimos la clase que permite las transiciones de colores
+      // para que no entre en conflicto con la animación de hover
+      group.classList.add("incorrectoClickado");
+      group.classList.add("incorrectoSeleccionado");
+      animandoObjeto = true;
       const tl = createTimeline({ defaults: { duration: 750 } });
 
       tl.label("start")
-        .add(
-          group,
-          {
-            // y: [
-            //   {
-            //     to: offsetY,
-            //     ease: "inOutQuad",
-            //     duration: 500,
-            //   },
-            // ],
-            // x: [
-            //   {
-            //     to: offsetX,
-            //     ease: "inOutQuad",
-            //     duration: 500,
-            //   },
-            // ],
-            scale: [{ to: "2", ease: "outElastic(1, 0.3)", duration: 1000 }],
-          },
-          "start",
-        )
-        .add(
-          drawable,
-          {
-            draw: ["0 0", "0 1"],
-            ease: "inOutQuad",
-            duration: 500,
-            delay: stagger(100),
-          },
-          200,
-        )
-        .add(
-          rect,
-          {
-            fill: "#ffffffff",
-            ease: "inOutQuad",
-            duration: 500,
-          },
-          400,
-        );
-    } else {
-      const tl = createTimeline({ defaults: { duration: 750 } });
-
-      tl.label("start")
-        .add(
-          drawable,
-          {
-            draw: ["0 1", "0 0"],
-            ease: "inOutQuad",
-            duration: 300,
-            // delay: stagger(100),
-          },
-          "start",
-        )
-        .add(
-          rect,
-          {
-            fill: "#ffffff00",
-            ease: "inOutQuad",
-            duration: 500,
-          },
-          200,
-        )
-        .add(
-          group,
-          {
-            y: [
-              {
-                to: 0,
-                ease: "inOutQuad",
-                duration: 500,
-              },
-            ],
-            x: [
-              {
-                to: 0,
-                ease: "inOutQuad",
-                duration: 500,
-              },
-            ],
-            scale: [{ to: "1", ease: "outElastic(1, 0.3)", duration: 1000 }],
-          },
-          300,
-        )
+        .add(group, {
+          rotate: "5deg",
+          ease: "inOutSine",
+          alternate: true,
+          loop: 2,
+          duration: 100,
+        })
+        .add(group, {
+          rotate: 0,
+          duration: 100,
+        })
         .call(() => {
-          // Devolvemos el grupo a su posición original
-          const referenceNode =
-            group.parentNode.children[originalIndex] || null;
-          group.parentNode.insertBefore(group, referenceNode);
+          group.classList.remove("incorrectoSeleccionado");
+        })
+        .call(() => {
+          group.classList.remove("incorrectoClickado");
+          animandoObjeto = false;
+        }, 500);
+    } else {
+      // OBJETO CORRECTO
 
-          rect.remove();
+      if (!animandoObjeto) {
+        animandoObjeto = true;
+        // Añade un cuadrado que rodea a cada grupo
+        const bbox = group.getBBox();
+        rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        rect.setAttribute("class", "rectangulo");
+        rect.setAttribute("x", bbox.x - 10);
+        rect.setAttribute("y", bbox.y - 10);
+        rect.setAttribute("width", bbox.width + 20);
+        rect.setAttribute("height", bbox.height + 20);
+        rect.setAttribute("fill", "#ffffff00"); // Transparente
+        rect.setAttribute("pointer-events", "all");
+        rect.setAttribute("stroke-width", 2);
+        rect.setAttribute("stroke", "#000000");
+        rect.setAttribute("rx", "5");
+        rect.setAttribute("ry", "5");
+        // Colocamos el rect al comienzo del grupo para que quede debajo de los elementos
+        group.insertBefore(rect, group.firstChild);
 
-          cuadradoDibujado = false;
-        }, 300);
+        drawable = svg.createDrawable(rect);
+
+        // Guardamos la posición de hijo original del grupo con respecto al padre
+        originalIndex = Array.from(group.parentNode.children).indexOf(group);
+
+        // Colocamos el grupo al final del padre para que quede por encima de los demás
+        group.parentNode.appendChild(group);
+        // svgPadre.appendChild(group);
+
+        // Colocamos el grupo en el centro de la pantalla
+        const groupRect = group.getBoundingClientRect();
+        const centerY = window.clientHeight / 2;
+        const centerX = window.clientWidth / 2;
+        const offsetY = centerY - (groupRect.top + groupRect.height / 2);
+        const offsetX = centerX - (groupRect.left + groupRect.width / 2);
+
+        const tl = createTimeline({ defaults: { duration: 750 } });
+
+        tl.label("start")
+          .add(
+            group,
+            {
+              // y: [
+              //   {
+              //     to: offsetY,
+              //     ease: "inOutQuad",
+              //     duration: 500,
+              //   },
+              // ],
+              // x: [
+              //   {
+              //     to: offsetX,
+              //     ease: "inOutQuad",
+              //     duration: 500,
+              //   },
+              // ],
+              scale: [{ to: "2", ease: "outElastic(1, 0.3)", duration: 1000 }],
+            },
+            "start",
+          )
+          .add(
+            drawable,
+            {
+              draw: ["0 0", "0 1"],
+              ease: "inOutQuad",
+              duration: 500,
+              delay: stagger(100),
+            },
+            200,
+          )
+          .add(
+            rect,
+            {
+              fill: "#ffffffff",
+              ease: "inOutQuad",
+              duration: 500,
+            },
+            400,
+          );
+      } else {
+        const tl = createTimeline({ defaults: { duration: 750 } });
+
+        tl.label("start")
+          .add(
+            drawable,
+            {
+              draw: ["0 1", "0 0"],
+              ease: "inOutQuad",
+              duration: 300,
+              // delay: stagger(100),
+            },
+            "start",
+          )
+          .add(
+            rect,
+            {
+              fill: "#ffffff00",
+              ease: "inOutQuad",
+              duration: 500,
+            },
+            200,
+          )
+          .add(
+            group,
+            {
+              y: [
+                {
+                  to: 0,
+                  ease: "inOutQuad",
+                  duration: 500,
+                },
+              ],
+              x: [
+                {
+                  to: 0,
+                  ease: "inOutQuad",
+                  duration: 500,
+                },
+              ],
+              scale: [{ to: "1", ease: "outElastic(1, 0.3)", duration: 1000 }],
+            },
+            300,
+          )
+          .call(() => {
+            // Devolvemos el grupo a su posición original
+            const referenceNode =
+              group.parentNode.children[originalIndex] || null;
+            group.parentNode.insertBefore(group, referenceNode);
+
+            rect.remove();
+
+            animandoObjeto = false;
+          }, 300);
+      }
     }
   });
 });
